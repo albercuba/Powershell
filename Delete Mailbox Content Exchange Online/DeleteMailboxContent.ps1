3025eabb216ee9e3da8216b760cf8017054054a2
albercuba <albercuba@gmail.com>
2021-10-13 12:51:23 +0200

    Create DeleteMailboxContent.ps1

---
+++ Delete Mailbox Content Exchange Online/DeleteMailboxContent.ps1
@@ -0,0 +1,24 @@
+##################################################
+# O365 delete Mailbox content in Exchange Online #
+##################################################
+
+# Import necessary module
+Import-Module ExchangeOnlineManagement
+
+# Use this to login to Exchange Online with a specific user (The user must have the proper permissions in Exchange)
+$login = "admin@tenant.onmicrosoft.com"
+
+# Use this to login to Exchange Online with the currently logged in user (The user must have the proper permissions in Exchange)
+#$login = whoami /upn
+
+# Connect to Exchange Online
+Connect-ExchangeOnline -UserPrincipalName $login
+
+# Declare the Mailbox variable
+$Mailbox = "user@domain.com"
+
+# Get the user Mailbox
+Get-MailboxFolderStatistics -Identity $Mailbox | Select FolderPath, Itemsfolder
+
+# Delete the content of the Mailbox
+Search-Mailbox -Identity "$Mailbox" -DeleteContent
