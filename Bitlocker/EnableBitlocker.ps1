d517630f58d9e3fc7ba34176ea89856645b8dd95
albercuba <albercuba@gmail.com>
2021-10-14 14:59:34 +0200

    Create EnableBitlocker.ps1

---
+++ Bitlocker/EnableBitlocker.ps1
@@ -0,0 +1,11 @@
+# Check if BitLocker is enabled in C:. If it is not, enable it and encrypt the drives
+
+$OutputVariable = (Get-BitlockerVolume -MountPoint "C:")
+If ($OutputVariable.protectionstatus -like "Off") 
+{
+    Enable-BitLocker C: -RecoveryPasswordProtector -SkipHardwareTest
+} 
+Else 
+{
+    Exit
+}
